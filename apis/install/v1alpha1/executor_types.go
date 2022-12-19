6865bdf6b3721f949822c16ce14c679b7c3f0d67
Geoff Wilson <geoff@gr-oss.io>
2022-12-19 17:43:51 -0500

    Fix module path for armadaproject

--- apis/install/v1alpha1/executor_types.go
+++ apis/install/v1alpha1/executor_types.go
@@ -17,7 +17,7 @@ limitations under the License.
 package v1alpha1
 
 import (
-	"github.com/dejanzele/armada-operator/apis/common"
+	"github.com/armadaproject/armada-operator/apis/common"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
