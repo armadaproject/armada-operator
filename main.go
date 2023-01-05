1893922d4e5b0815923df11b7389a7d404fbe068
Clif Houck <me@clifhouck.com>
2023-01-05 11:31:08 -0600

    Revert "Immediate aftermath of operator-sdk create api for LookoutIngester"
    This reverts commit 666f7435607de4957723893dcfbd7ce10bb3ef6e.

--- main.go
+++ main.go
@@ -32,7 +32,6 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
