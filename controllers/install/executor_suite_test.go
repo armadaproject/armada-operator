603d07e19a16c9dc4c86030e6d4a4e99cbe72b27
Geoff Wilson <geoff@gr-oss.io>
2023-01-06 11:26:42 -0500

    compilation after merge fixes

--- controllers/install/executor_suite_test.go
+++ controllers/install/executor_suite_test.go
@@ -18,7 +18,6 @@ package install
 
 import (
 	"context"
-	installv1alpha1 "github.com/armadaproject/armada-operator/apis/install/v1alpha1"
 	"os"
 	"path/filepath"
 	ctrl "sigs.k8s.io/controller-runtime"
