92bad2a98c3e156be91019405c2cd600aa0486c8
Kevin Hannon <kannon1992@gmail.com>
2023-01-10 17:28:42 -0500

    server unit tests

---
+++ controllers/install/server_controller_test.go
@@ -0,0 +1,39 @@
+package install
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"github.com/armadaproject/armada-operator/apis/install/v1alpha1"
+	"github.com/armadaproject/armada-operator/internal/k8sclient"
+)
+
+func TestServerReconciler_Reconcile(t *testing.T) {
+	t.Parallel()
+
+	mockCtrl := gomock.NewController(t)
+	defer mockCtrl.Finish()
+	scheme, err := v1alpha1.SchemeBuilder.Build()
+	if err != nil {
+		t.Fatalf("should not return error when building schema")
+	}
+	mockK8sClient := k8sclient.NewMockClient(mockCtrl)
+
+	r := ServerReconciler{
+		Client: mockK8sClient,
+		Scheme: scheme,
+	}
+
+	req := ctrl.Request{
+		NamespacedName: types.NamespacedName{Namespace: "default", Name: "binoculars"},
+	}
+
+	_, err = r.Reconcile(context.Background(), req)
+	if err != nil {
+		t.Fatalf("reconcile should not return error")
+	}
+}
