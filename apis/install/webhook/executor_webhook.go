da95d1e89c94e709f0c8d71caa01ca86aaa66884
Kevin Hannon <kannon1992@gmail.com>
2023-01-10 17:19:48 -0500

    webhook

--- apis/install/webhook/executor_webhook.go
+++ apis/install/webhook/executor_webhook.go
@@ -28,13 +28,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
-const (
-	keySpec              = "spec"
-	keyApplicationConfig = "applicationConfig"
-	keyAPIConnection     = "apiConnection"
-	keyArmadaURL         = "armadaUrl"
-)
-
 // log is for logging in this package.
 var executorlog = logf.Log.WithName("executor-resource")
 
