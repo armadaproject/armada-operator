96c6b24a1d916928294ecf7bcd3cc67f7b3b5f50
Dejan Pejchev <pejcev.dejan@gmail.com>
2023-01-12 00:42:09 +0100

    add unit and e2e test for deleting Executor

--- test/integration/executor_controller_e2e_test.go
+++ test/integration/executor_controller_e2e_test.go
@@ -2,6 +2,9 @@ package integration
 
 import (
 	"io"
+	rbacv1 "k8s.io/api/rbac/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"net/http"
 	"os"
 	"time"
 
@@ -38,12 +41,8 @@ spec:
 var executorYaml2 = `apiVersion: install.armadaproject.io/v1alpha1
 kind: Executor
 metadata:
-  labels:
-    app.kubernetes.io/name: executor
-    app.kubernetes.io/instance: executor-sample
-    app.kubernetes.io/part-of: armada-operator
-    app.kubernetes.io/created-by: armada-operator
   name: executor-e2e-2
+  namespace: default
 spec:
   image:
     repository: test-executor
@@ -60,12 +59,25 @@ var executorYaml2Updated = `apiVersion: install.armadaproject.io/v1alpha1
 kind: Executor
 metadata:
   labels:
-    app.kubernetes.io/name: executor
-    app.kubernetes.io/instance: executor-sample
-    app.kubernetes.io/part-of: armada-operator
-    app.kubernetes.io/created-by: armada-operator
     test: updated
   name: executor-e2e-2
+  namespace: default
+spec:
+  image:
+    repository: test-executor
+    tag: latest
+  applicationConfig:
+    server: example.com:443
+    forceNoTls: true
+    toleratedTaints:
+      - key: armada.io/batch
+        operator: in
+`
+
+var executorYaml3 = `apiVersion: install.armadaproject.io/v1alpha1
+kind: Executor
+metadata:
+  name: executor-e2e-3
 spec:
   image:
     repository: test-executor
@@ -122,6 +134,16 @@ var _ = Describe("Executor Controller", func() {
 				serviceKey := kclient.ObjectKey{Namespace: "default", Name: "executor-e2e-1"}
 				err = k8sClient.Get(ctx, serviceKey, &service)
 				Expect(err).NotTo(HaveOccurred())
+
+				clusterRole := rbacv1.ClusterRole{}
+				clusterRoleKey := kclient.ObjectKey{Namespace: "", Name: "executor-e2e-1"}
+				err = k8sClient.Get(ctx, clusterRoleKey, &clusterRole)
+				Expect(err).NotTo(HaveOccurred())
+
+				clusterRoleBinding := rbacv1.ClusterRoleBinding{}
+				clusterRoleBindingKey := kclient.ObjectKey{Namespace: "", Name: "executor-e2e-3"}
+				err = k8sClient.Get(ctx, clusterRoleBindingKey, &clusterRoleBinding)
+				Expect(err).NotTo(HaveOccurred())
 			})
 		})
 	})
@@ -174,6 +196,85 @@ var _ = Describe("Executor Controller", func() {
 				err = k8sClient.Get(ctx, executorKey, &executor)
 				Expect(err).NotTo(HaveOccurred())
 				Expect(executor.Labels["test"]).To(BeEquivalentTo("updated"))
+			})
+		})
+	})
+
+	When("User deletes an existing Executor YAML using kubectl", func() {
+		It("Kubernetes should delete the Executor Kubernetes resources", func() {
+			By("Calling the Executor Controller Reconcile function", func() {
+				f, err := CreateTempFile([]byte(executorYaml3))
+				Expect(err).ToNot(HaveOccurred())
+				defer f.Close()
+				defer os.Remove(f.Name())
+
+				k, err := testUser.Kubectl()
+				Expect(err).ToNot(HaveOccurred())
+				stdin, stderr, err := k.Run("apply", "-f", f.Name())
+				if err != nil {
+					stderrBytes, err := io.ReadAll(stderr)
+					Expect(err).ToNot(HaveOccurred())
+					Fail(string(stderrBytes))
+				}
+				stdinBytes, err := io.ReadAll(stdin)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(string(stdinBytes)).To(BeEquivalentTo("executor.install.armadaproject.io/executor-e2e-3 created\n"))
+
+				time.Sleep(1 * time.Second)
+
+				stdin, stderr, err = k.Run("delete", "-f", f.Name())
+				if err != nil {
+					stderrBytes, err := io.ReadAll(stderr)
+					Expect(err).ToNot(HaveOccurred())
+					Fail(string(stderrBytes))
+				}
+				stdinBytes, err = io.ReadAll(stdin)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(string(stdinBytes)).To(Equal("executor.install.armadaproject.io \"executor-e2e-3\" deleted\n"))
+
+				time.Sleep(2 * time.Second)
+
+				executor := installv1alpha1.Executor{}
+				executorKey := kclient.ObjectKey{Namespace: "default", Name: "executor-e2e-1"}
+				err = k8sClient.Get(ctx, executorKey, &executor)
+				Expect(err).To(BeAssignableToTypeOf(&errors.StatusError{}))
+				notFoundErr := err.(*errors.StatusError)
+				Expect(notFoundErr.ErrStatus.Code).To(BeEquivalentTo(http.StatusNotFound))
+
+				secret := corev1.Secret{}
+				secretKey := kclient.ObjectKey{Namespace: "default", Name: "executor-e2e-1"}
+				err = k8sClient.Get(ctx, secretKey, &secret)
+				Expect(err).To(BeAssignableToTypeOf(&errors.StatusError{}))
+				notFoundErr = err.(*errors.StatusError)
+				Expect(notFoundErr.ErrStatus.Code).To(BeEquivalentTo(http.StatusNotFound))
+
+				deployment := appsv1.Deployment{}
+				deploymentKey := kclient.ObjectKey{Namespace: "default", Name: "executor-e2e-1"}
+				err = k8sClient.Get(ctx, deploymentKey, &deployment)
+				Expect(err).To(BeAssignableToTypeOf(&errors.StatusError{}))
+				notFoundErr = err.(*errors.StatusError)
+				Expect(notFoundErr.ErrStatus.Code).To(BeEquivalentTo(http.StatusNotFound))
+
+				service := corev1.Service{}
+				serviceKey := kclient.ObjectKey{Namespace: "default", Name: "executor-e2e-1"}
+				err = k8sClient.Get(ctx, serviceKey, &service)
+				Expect(err).To(BeAssignableToTypeOf(&errors.StatusError{}))
+				notFoundErr = err.(*errors.StatusError)
+				Expect(notFoundErr.ErrStatus.Code).To(BeEquivalentTo(http.StatusNotFound))
+
+				clusterRole := rbacv1.ClusterRole{}
+				clusterRoleKey := kclient.ObjectKey{Namespace: "", Name: "executor-e2e-1"}
+				err = k8sClient.Get(ctx, clusterRoleKey, &clusterRole)
+				Expect(err).To(BeAssignableToTypeOf(&errors.StatusError{}))
+				notFoundErr = err.(*errors.StatusError)
+				Expect(notFoundErr.ErrStatus.Code).To(BeEquivalentTo(http.StatusNotFound))
+
+				clusterRoleBinding := rbacv1.ClusterRoleBinding{}
+				clusterRoleBindingKey := kclient.ObjectKey{Namespace: "", Name: "executor-e2e-3"}
+				err = k8sClient.Get(ctx, clusterRoleBindingKey, &clusterRoleBinding)
+				Expect(err).To(BeAssignableToTypeOf(&errors.StatusError{}))
+				notFoundErr = err.(*errors.StatusError)
+				Expect(notFoundErr.ErrStatus.Code).To(BeEquivalentTo(http.StatusNotFound))
 			})
 		})
 	})
