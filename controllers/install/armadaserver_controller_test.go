9b3b1ca20e6afeadf3ff2717b23c61510847718b
Rich Scott <richscott@sent.com>
2023-01-30 16:26:44 -0700

    Get test mock expectations for pulsar-wait job working.

--- controllers/install/armadaserver_controller_test.go
+++ controllers/install/armadaserver_controller_test.go
@@ -8,7 +8,9 @@ import (
 	"github.com/golang/mock/gomock"
 	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	appsv1 "k8s.io/api/apps/v1"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
 	policyv1 "k8s.io/api/policy/v1"
@@ -19,9 +21,9 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 
-	"github.com/armadaproject/armada-operator/apis/install/v1alpha1"
 	"github.com/armadaproject/armada-operator/test/k8sclient"
 
+	"github.com/armadaproject/armada-operator/apis/install/v1alpha1"
 	installv1alpha1 "github.com/armadaproject/armada-operator/apis/install/v1alpha1"
 )
 
@@ -31,14 +33,21 @@ func TestArmadaServerReconciler_Reconcile(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
+	scheme, err := v1alpha1.SchemeBuilder.Build()
+	if err != nil {
+		t.Fatalf("should not return error when building schema")
+	}
+
 	expectedNS := types.NamespacedName{Namespace: "default", Name: "armadaserver"}
-	expectedAS := v1alpha1.ArmadaServer{
+	migrationNS := types.NamespacedName{Namespace: "default", Name: "armadaserver-migration"}
+
+	expectedAS := installv1alpha1.ArmadaServer{
 		TypeMeta: metav1.TypeMeta{
 			Kind:       "ArmadaServer",
 			APIVersion: "install.armadaproject.io/v1alpha1",
 		},
 		ObjectMeta: metav1.ObjectMeta{Namespace: "default", Name: "armadaserver"},
-		Spec: v1alpha1.ArmadaServerSpec{
+		Spec: installv1alpha1.ArmadaServerSpec{
 			Labels: map[string]string{"test": "hello"},
 			Image: installv1alpha1.Image{
 				Repository: "testrepo",
@@ -56,14 +65,40 @@ func TestArmadaServerReconciler_Reconcile(t *testing.T) {
 		},
 	}
 
+	expectedComponents, err := generateArmadaServerInstallComponents(&expectedAS, scheme)
+	require.NoError(t, err)
+
 	mockK8sClient := k8sclient.NewMockClient(mockCtrl)
 
 	// Armada Server
 	mockK8sClient.
 		EXPECT().
-		Get(gomock.Any(), expectedNS, gomock.AssignableToTypeOf(&v1alpha1.ArmadaServer{})).
+		Get(gomock.Any(), expectedNS, gomock.AssignableToTypeOf(&installv1alpha1.ArmadaServer{})).
 		Return(nil).
 		SetArg(2, expectedAS)
+
+	// Pulsar migration job
+	expectedComponents.Job.Status = batchv1.JobStatus{
+		Conditions: []batchv1.JobCondition{{
+			Type:   batchv1.JobComplete,
+			Status: corev1.ConditionTrue,
+		}},
+	}
+	mockK8sClient.
+		EXPECT().
+		Get(gomock.Any(), migrationNS, gomock.AssignableToTypeOf(&batchv1.Job{})).
+		Return(errors.NewNotFound(schema.GroupResource{}, "armadaserver-migration"))
+	mockK8sClient.
+		EXPECT().
+		Create(gomock.Any(), gomock.AssignableToTypeOf(&batchv1.Job{})).
+		Return(nil).
+		SetArg(1, *expectedComponents.Job)
+	mockK8sClient.
+		EXPECT().
+		Get(gomock.Any(), migrationNS, gomock.AssignableToTypeOf(&batchv1.Job{})).
+		Return(nil).
+		SetArg(2, *expectedComponents.Job)
+
 	// Deployment
 	mockK8sClient.
 		EXPECT().
@@ -149,11 +184,6 @@ func TestArmadaServerReconciler_Reconcile(t *testing.T) {
 		EXPECT().
 		Create(gomock.Any(), gomock.AssignableToTypeOf(&monitoringv1.ServiceMonitor{})).
 		Return(nil)
-
-	scheme, err := v1alpha1.SchemeBuilder.Build()
-	if err != nil {
-		t.Fatalf("should not return error when building schema")
-	}
 
 	r := ArmadaServerReconciler{
 		Client: mockK8sClient,
