823e54ba16f5a5b0b7d8a1ca9902b93f3ca46b99
Geoff Wilson <geoff@gr-oss.io>
2023-02-07 11:07:20 +0000

    golangci-lint fix

--- main.go
+++ main.go
@@ -17,194 +17,194 @@ limitations under the License.
 package main
 
 import (
-    "flag"
-    "os"
+	"flag"
+	"os"
 
-    monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 
-    "github.com/armadaproject/armada-operator/controllers/install"
+	"github.com/armadaproject/armada-operator/controllers/install"
 
-    "github.com/armadaproject/armada-operator/apis/install/v1alpha1"
+	"github.com/armadaproject/armada-operator/apis/install/v1alpha1"
 
-    // Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-    // to ensure that exec-entrypoint and run can make use of them.
-    _ "k8s.io/client-go/plugin/pkg/client/auth"
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
-    "k8s.io/apimachinery/pkg/runtime"
-    utilruntime "k8s.io/apimachinery/pkg/util/runtime"
-    clientgoscheme "k8s.io/client-go/kubernetes/scheme"
-    ctrl "sigs.k8s.io/controller-runtime"
-    "sigs.k8s.io/controller-runtime/pkg/healthz"
-    "sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
-    corev1alpha1 "github.com/armadaproject/armada-operator/apis/core/v1alpha1"
-    installv1alpha1 "github.com/armadaproject/armada-operator/apis/install/v1alpha1"
-    corecontrollers "github.com/armadaproject/armada-operator/controllers/core"
-    //+kubebuilder:scaffold:imports
+	corev1alpha1 "github.com/armadaproject/armada-operator/apis/core/v1alpha1"
+	installv1alpha1 "github.com/armadaproject/armada-operator/apis/install/v1alpha1"
+	corecontrollers "github.com/armadaproject/armada-operator/controllers/core"
+	//+kubebuilder:scaffold:imports
 )
 
 var (
-    scheme   = runtime.NewScheme()
-    setupLog = ctrl.Log.WithName("setup")
+	scheme   = runtime.NewScheme()
+	setupLog = ctrl.Log.WithName("setup")
 )
 
 var (
-    version = "dev"
-    commit  = "none"
-    date    = "unknown"
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
 )
 
 func init() {
-    utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
-    utilruntime.Must(v1alpha1.AddToScheme(scheme))
-    utilruntime.Must(corev1alpha1.AddToScheme(scheme))
-    utilruntime.Must(installv1alpha1.AddToScheme(scheme))
-    //+kubebuilder:scaffold:scheme
+	utilruntime.Must(v1alpha1.AddToScheme(scheme))
+	utilruntime.Must(corev1alpha1.AddToScheme(scheme))
+	utilruntime.Must(installv1alpha1.AddToScheme(scheme))
+	//+kubebuilder:scaffold:scheme
 }
 
 func main() {
-    var metricsAddr string
-    var enableLeaderElection bool
-    var probeAddr string
-    flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-    flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-    flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-        "Enable leader election for controller manager. "+
-            "Enabling this will ensure there is only one active controller manager.")
-    opts := zap.Options{
-        Development: true,
-    }
-    opts.BindFlags(flag.CommandLine)
-    flag.Parse()
+	var metricsAddr string
+	var enableLeaderElection bool
+	var probeAddr string
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
+		"Enable leader election for controller manager. "+
+			"Enabling this will ensure there is only one active controller manager.")
+	opts := zap.Options{
+		Development: true,
+	}
+	opts.BindFlags(flag.CommandLine)
+	flag.Parse()
 
-    setupLog.Info("initializing armada-operator", "version", version, "commit", commit, "buildDate", date)
+	setupLog.Info("initializing armada-operator", "version", version, "commit", commit, "buildDate", date)
 
-    ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
-    mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-        Scheme:                 scheme,
-        MetricsBindAddress:     metricsAddr,
-        Port:                   9443,
-        HealthProbeBindAddress: probeAddr,
-        LeaderElection:         enableLeaderElection,
-        LeaderElectionID:       "b00891aa.armadaproject.io",
-        Logger:                 setupLog,
-        // LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
-        // when the Manager ends. This requires the binary to immediately end when the
-        // Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
-        // speeds up voluntary leader transitions as the new leader don't have to wait
-        // LeaseDuration time first.
-        //
-        // In the default scaffold provided, the program ends immediately after
-        // the manager stops, so would be fine to enable this option. However,
-        // if you are doing or is intended to do any operation such as perform cleanups
-        // after the manager stops then its usage might be unsafe.
-        // LeaderElectionReleaseOnCancel: true,
-    })
-    if err != nil {
-        setupLog.Error(err, "unable to start manager")
-        os.Exit(1)
-    }
+	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
+		Scheme:                 scheme,
+		MetricsBindAddress:     metricsAddr,
+		Port:                   9443,
+		HealthProbeBindAddress: probeAddr,
+		LeaderElection:         enableLeaderElection,
+		LeaderElectionID:       "b00891aa.armadaproject.io",
+		Logger:                 setupLog,
+		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
+		// when the Manager ends. This requires the binary to immediately end when the
+		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
+		// speeds up voluntary leader transitions as the new leader don't have to wait
+		// LeaseDuration time first.
+		//
+		// In the default scaffold provided, the program ends immediately after
+		// the manager stops, so would be fine to enable this option. However,
+		// if you are doing or is intended to do any operation such as perform cleanups
+		// after the manager stops then its usage might be unsafe.
+		// LeaderElectionReleaseOnCancel: true,
+	})
+	if err != nil {
+		setupLog.Error(err, "unable to start manager")
+		os.Exit(1)
+	}
 
-    if err := monitoringv1.AddToScheme(mgr.GetScheme()); err != nil {
-        setupLog.Error(err, "error registering monitoring.coreos.com/v1 schema")
-        os.Exit(1)
-    }
+	if err := monitoringv1.AddToScheme(mgr.GetScheme()); err != nil {
+		setupLog.Error(err, "error registering monitoring.coreos.com/v1 schema")
+		os.Exit(1)
+	}
 
-    if err = (&install.ArmadaServerReconciler{
-        Client: mgr.GetClient(),
-        Scheme: mgr.GetScheme(),
-    }).SetupWithManager(mgr); err != nil {
-        setupLog.Error(err, "unable to create controller", "controller", "Server")
-        os.Exit(1)
-    }
-    if err = (&install.ExecutorReconciler{
-        Client: mgr.GetClient(),
-        Scheme: mgr.GetScheme(),
-    }).SetupWithManager(mgr); err != nil {
-        setupLog.Error(err, "unable to create controller", "controller", "Executor")
-        os.Exit(1)
-    }
-    if err = (&install.BinocularsReconciler{
-        Client: mgr.GetClient(),
-        Scheme: mgr.GetScheme(),
-    }).SetupWithManager(mgr); err != nil {
-        setupLog.Error(err, "unable to create controller", "controller", "Binoculars")
-        os.Exit(1)
-    }
-    if err = (&install.LookoutReconciler{
-        Client: mgr.GetClient(),
-        Scheme: mgr.GetScheme(),
-    }).SetupWithManager(mgr); err != nil {
-        setupLog.Error(err, "unable to create controller", "controller", "Lookout")
-        os.Exit(1)
-    }
-    if err = (&install.EventIngesterReconciler{
-        Client: mgr.GetClient(),
-        Scheme: mgr.GetScheme(),
-    }).SetupWithManager(mgr); err != nil {
-        setupLog.Error(err, "unable to create controller", "controller", "EventIngester")
-        os.Exit(1)
-    }
+	if err = (&install.ArmadaServerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Server")
+		os.Exit(1)
+	}
+	if err = (&install.ExecutorReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Executor")
+		os.Exit(1)
+	}
+	if err = (&install.BinocularsReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Binoculars")
+		os.Exit(1)
+	}
+	if err = (&install.LookoutReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Lookout")
+		os.Exit(1)
+	}
+	if err = (&install.EventIngesterReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "EventIngester")
+		os.Exit(1)
+	}
 
-    if err = (&corecontrollers.QueueReconciler{
-        Client: mgr.GetClient(),
-        Scheme: mgr.GetScheme(),
-    }).SetupWithManager(mgr); err != nil {
-        setupLog.Error(err, "unable to create controller", "controller", "Queue")
-        os.Exit(1)
-    }
-    if err = (&install.LookoutIngesterReconciler{
-        Client: mgr.GetClient(),
-        Scheme: mgr.GetScheme(),
-    }).SetupWithManager(mgr); err != nil {
-        setupLog.Error(err, "unable to create controller", "controller", "LookoutIngester")
-        os.Exit(1)
-    }
+	if err = (&corecontrollers.QueueReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Queue")
+		os.Exit(1)
+	}
+	if err = (&install.LookoutIngesterReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "LookoutIngester")
+		os.Exit(1)
+	}
 
-    disableWebhooks := os.Getenv("DISABLE_WEBHOOKS") == "true"
-    if !disableWebhooks {
-        if err = (&installv1alpha1.ArmadaServer{}).SetupWebhookWithManager(mgr); err != nil {
-            setupLog.Error(err, "unable to create webhook", "webhook", "ArmadaServer")
-            os.Exit(1)
-        }
-        if err = (&v1alpha1.Executor{}).SetupWebhookWithManager(mgr); err != nil {
-            setupLog.Error(err, "unable to create webhook", "webhook", "Executor")
-            os.Exit(1)
-        }
-        if err = (&installv1alpha1.EventIngester{}).SetupWebhookWithManager(mgr); err != nil {
-            setupLog.Error(err, "unable to create webhook", "webhook", "EventIngester")
-            os.Exit(1)
-        }
-        if err = (&installv1alpha1.Binoculars{}).SetupWebhookWithManager(mgr); err != nil {
-            setupLog.Error(err, "unable to create webhook", "webhook", "Binoculars")
-            os.Exit(1)
-        }
-        if err = (&installv1alpha1.Lookout{}).SetupWebhookWithManager(mgr); err != nil {
-            setupLog.Error(err, "unable to create webhook", "webhook", "Lookout")
-            os.Exit(1)
-        }
-        if err = (&installv1alpha1.LookoutIngester{}).SetupWebhookWithManager(mgr); err != nil {
-            setupLog.Error(err, "unable to create webhook", "webhook", "LookoutIngester")
-            os.Exit(1)
-        }
-    }
+	disableWebhooks := os.Getenv("DISABLE_WEBHOOKS") == "true"
+	if !disableWebhooks {
+		if err = (&installv1alpha1.ArmadaServer{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "ArmadaServer")
+			os.Exit(1)
+		}
+		if err = (&v1alpha1.Executor{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Executor")
+			os.Exit(1)
+		}
+		if err = (&installv1alpha1.EventIngester{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "EventIngester")
+			os.Exit(1)
+		}
+		if err = (&installv1alpha1.Binoculars{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Binoculars")
+			os.Exit(1)
+		}
+		if err = (&installv1alpha1.Lookout{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Lookout")
+			os.Exit(1)
+		}
+		if err = (&installv1alpha1.LookoutIngester{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "LookoutIngester")
+			os.Exit(1)
+		}
+	}
 
-    //+kubebuilder:scaffold:builder
+	//+kubebuilder:scaffold:builder
 
-    if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
-        setupLog.Error(err, "unable to set up health check")
-        os.Exit(1)
-    }
-    if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
-        setupLog.Error(err, "unable to set up ready check")
-        os.Exit(1)
-    }
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up health check")
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		setupLog.Error(err, "unable to set up ready check")
+		os.Exit(1)
+	}
 
-    setupLog.Info("starting manager")
-    if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
-        setupLog.Error(err, "problem running manager")
-        os.Exit(1)
-    }
+	setupLog.Info("starting manager")
+	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		setupLog.Error(err, "problem running manager")
+		os.Exit(1)
+	}
 }
