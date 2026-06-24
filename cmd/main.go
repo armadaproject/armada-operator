eb66ca6f1f544e29082f4b0eecce3abf50855d12
Dejan Zele Pejchev <pejcev.dejan@gmail.com>
2026-06-24 13:18:59 +0200

    fix: replace kube-rbac-proxy with built-in metrics authn/authz
    The kube-rbac-proxy sidecar pulled docker.io/kubebuilder/kube-rbac-proxy:v0.16.0,
    a deprecated, amd64-only Docker Hub mirror that fails to pull on other
    architectures. kubebuilder discontinued kube-rbac-proxy as of v4.1.0 in favour
    of controller-runtime's built-in metrics authn/authz.
    
    Migrate to the modern pattern:
    - Serve metrics over HTTPS on :8443 with SecureServing and the
      WithAuthenticationAndAuthorization filter provider in cmd/main.go.
    - Remove the sidecar container and its image from the Helm chart and kustomize
      config; expose the metrics port on the manager container instead.
    - Replace the auth_proxy_* RBAC with metrics_auth and metrics_reader roles
      (same TokenReview/SubjectAccessReview permissions, now used by the manager SA).
    
    This removes the dependency on the broken external image entirely.
    
    Signed-off-by: Dejan Zele Pejchev <pejcev.dejan@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -17,6 +17,7 @@ limitations under the License.
 package main
 
 import (
+	"crypto/tls"
 	"flag"
 	"os"
 
@@ -38,6 +39,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
 	corev1alpha1 "github.com/armadaproject/armada-operator/api/core/v1alpha1"
@@ -71,12 +73,19 @@ func main() {
 	var enableLeaderElection bool
 	var probeAddr string
 	var pprofAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	var secureMetrics bool
+	var enableHTTP2 bool
+	var tlsOpts []func(*tls.Config)
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8443", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.StringVar(&pprofAddr, "pprof-bind-address", "", "The address the pprof endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
+	flag.BoolVar(&secureMetrics, "metrics-secure", true,
+		"If set, the metrics endpoint is served securely via HTTPS. Use --metrics-secure=false to use HTTP instead.")
+	flag.BoolVar(&enableHTTP2, "enable-http2", false,
+		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
 	opts := zap.Options{
 		Development: true,
 	}
@@ -87,13 +96,47 @@ func main() {
 
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
+	// if the enable-http2 flag is false (the default), http/2 should be disabled
+	// due to its vulnerabilities. More specifically, disabling http/2 will
+	// prevent from being vulnerable to the HTTP/2 Stream Cancellation and
+	// Rapid Reset CVEs. For more information see:
+	// - https://github.com/advisories/GHSA-qppj-fm5r-hxr3
+	// - https://github.com/advisories/GHSA-4374-p667-p6c8
+	disableHTTP2 := func(c *tls.Config) {
+		setupLog.Info("Disabling HTTP/2")
+		c.NextProtos = []string{"http/1.1"}
+	}
+	if !enableHTTP2 {
+		tlsOpts = append(tlsOpts, disableHTTP2)
+	}
+
 	webhookOptions := webhook.Options{
 		CertDir: os.Getenv("WEBHOOK_CERT_DIR"),
+		TLSOpts: tlsOpts,
 	}
 	webhookServer := webhook.NewServer(webhookOptions)
+
+	// Metrics endpoint is enabled in 'config/default/kustomization.yaml'. The Metrics
+	// options configure the server. More info:
+	// - https://pkg.go.dev/sigs.k8s.io/controller-runtime/pkg/metrics/server
+	// - https://book.kubebuilder.io/reference/metrics.html
+	metricsServerOptions := metricsserver.Options{
+		BindAddress:   metricsAddr,
+		SecureServing: secureMetrics,
+		TLSOpts:       tlsOpts,
+	}
+	if secureMetrics {
+		// FilterProvider is used to protect the metrics endpoint with authn/authz.
+		// These configurations ensure that only authorized users and service accounts
+		// can access the metrics endpoint. The RBAC are configured in
+		// 'config/rbac/kustomization.yaml'. More info:
+		// https://pkg.go.dev/sigs.k8s.io/controller-runtime/pkg/metrics/filters#WithAuthenticationAndAuthorization
+		metricsServerOptions.FilterProvider = filters.WithAuthenticationAndAuthorization
+	}
+
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
-		Metrics:                metricsserver.Options{BindAddress: metricsAddr},
+		Metrics:                metricsServerOptions,
 		HealthProbeBindAddress: probeAddr,
 		PprofBindAddress:       pprofAddr,
 		LeaderElection:         enableLeaderElection,
