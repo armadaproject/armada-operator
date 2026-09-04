363c414455e3448181a7dfe69ba5bc38f8a40e64
Dejan Zele Pejchev <pejcev.dejan@gmail.com>
2026-09-04 17:14:11 +0200

    chore: replace deprecated reflect.Ptr with reflect.Pointer (#388)
    Signed-off-by: Dejan Zele Pejchev <pejcev.dejan@gmail.com>

--- internal/controller/install/common_helpers.go
+++ internal/controller/install/common_helpers.go
@@ -637,7 +637,7 @@ func isNil(i any) bool {
 		return true
 	}
 	switch iv.Kind() {
-	case reflect.Ptr, reflect.Slice, reflect.Map, reflect.Func, reflect.Interface:
+	case reflect.Pointer, reflect.Slice, reflect.Map, reflect.Func, reflect.Interface:
 		return iv.IsNil()
 	default:
 		return false
